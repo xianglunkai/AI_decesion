d7bea8d33b5204ed4874c8c87b6ac699c07440e2
xianglunkai <1322099375@qq.com>
2023-07-26 11:30:28 +0800

    add osqp

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -3,15 +3,15 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-g++)
+# SET(CMAKE_C_COMPILER   /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc)
+# SET(CMAKE_CXX_COMPILER /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-g++)
 
-# SET(CMAKE_C_COMPILER   /opt/arm-2013.11/bin/arm-none-linux-gnueabi-gcc)
-# SET(CMAKE_CXX_COMPILER /opt/arm-2013.11/bin/arm-none-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER   /opt/arm-2013.11/bin/arm-none-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-2013.11/bin/arm-none-linux-gnueabi-g++)
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi)
-# SET(CMAKE_FIND_ROOT_PATH  /opt/arm-2013.11)
+# SET(CMAKE_FIND_ROOT_PATH  /opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi)
+SET(CMAKE_FIND_ROOT_PATH  /opt/arm-2013.11)
 
 # search for programs in the build host directories (not necessary)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
